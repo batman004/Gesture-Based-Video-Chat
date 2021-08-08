3dbdab4fed2edf6279c0ee5dbc5c0c9d40cd4c16
batman004 <pathania.yuvraj@gmail.com>
2021-08-08 07:09:49 +0530

    components working

---
+++ agora.js
@@ -0,0 +1,218 @@
+/*
+ * JS Interface for Agora.io SDK
+ */
+
+// video profile settings
+
+var cameraVideoProfile = '480p_4'; // 640 × 480 @ 30fps  & 750kbs
+var screenVideoProfile = '480p_2'; // 640 × 480 @ 30fps
+
+// create client instances for camera (client) 
+var client = AgoraRTC.createClient({mode: 'rtc', codec: 'vp8'}); 
+
+
+// stream references (keep track of active streams) 
+var remoteStreams = {}; // remote streams obj struct [id : stream] 
+
+var localStreams = {
+  camera: {
+    id: "",
+    stream: {}
+  },
+  screen: {
+    id: "",
+    stream: {}
+  }
+};
+
+AgoraRTC.Logger.enableLogUpload(); // auto upload logs
+
+var mainStreamId; // reference to main stream
+
+
+function initClientAndJoinChannel(agoraAppId, token, channelName, uid) {
+  // init Agora SDK
+  client.init(agoraAppId, function () {
+    console.log("AgoraRTC client initialized");
+    joinChannel(channelName, uid, token); // join channel upon successfull init
+  }, function (err) {
+    console.log("[ERROR] : AgoraRTC client init failed", err);
+  });
+}
+
+
+client.on('stream-published', function (evt) {
+  console.log("Publish local stream successfully");
+});
+
+// connect remote streams
+client.on('stream-added', function (evt) {
+  var stream = evt.stream;
+  var streamId = stream.getId();
+  console.log("new stream added: " + streamId);
+  // Check if the stream is local
+  if (streamId != localStreams.screen.id) {
+    console.log('subscribe to remote stream:' + streamId);
+    // Subscribe to the stream.
+    client.subscribe(stream, function (err) {
+      console.log("[ERROR] : subscribe stream failed", err);
+    });
+  }
+});
+
+client.on('stream-subscribed', function (evt) {
+  var remoteStream = evt.stream;
+  var remoteId = remoteStream.getId();
+  remoteStreams[remoteId] = remoteStream;
+  console.log("Subscribe remote stream successfully: " + remoteId);
+  if( $('#full-screen-video').is(':empty') ) { 
+    mainStreamId = remoteId;
+    remoteStream.play('full-screen-video');
+    $('#main-stats-btn').show();
+    $('#main-stream-stats-btn').show();
+  } else if (remoteId == 49024) {
+    // move the current main stream to miniview
+    remoteStreams[mainStreamId].stop(); // stop the main video stream playback
+    client.setRemoteVideoStreamType(remoteStreams[mainStreamId], 1); // subscribe to the low stream
+    addRemoteStreamMiniView(remoteStreams[mainStreamId]); // send the main video stream to a container
+    // set the screen-share as the main 
+    mainStreamId = remoteId;
+    remoteStream.play('full-screen-video');s
+  } else {
+    client.setRemoteVideoStreamType(remoteStream, 1); // subscribe to the low stream
+    addRemoteStreamMiniView(remoteStream);
+  }
+});
+
+// remove the remote-container when a user leaves the channel
+client.on("peer-leave", function(evt) {
+  var streamId = evt.stream.getId(); // the the stream id
+  if(remoteStreams[streamId] != undefined) {
+    remoteStreams[streamId].stop(); // stop playing the feed
+    delete remoteStreams[streamId]; // remove stream from list
+    if (streamId == mainStreamId) {
+      var streamIds = Object.keys(remoteStreams);
+      var randomId = streamIds[Math.floor(Math.random()*streamIds.length)]; // select from the remaining streams
+      remoteStreams[randomId].stop(); // stop the stream's existing playback
+      var remoteContainerID = '#' + randomId + '_container';
+      $(remoteContainerID).empty().remove(); // remove the stream's miniView container
+      remoteStreams[randomId].play('full-screen-video'); // play the random stream as the main stream
+      mainStreamId = randomId; // set the new main remote stream
+    } else {
+      var remoteContainerID = '#' + streamId + '_container';
+      $(remoteContainerID).empty().remove(); // 
+    }
+  }
+});
+
+// show mute icon whenever a remote has muted their mic
+client.on("mute-audio", function (evt) {
+  toggleVisibility('#' + evt.uid + '_mute', true);
+});
+
+client.on("unmute-audio", function (evt) {
+  toggleVisibility('#' + evt.uid + '_mute', false);
+});
+
+// show user icon whenever a remote has disabled their video
+client.on("mute-video", function (evt) {
+  var remoteId = evt.uid;
+  // if the main user stops their video select a random user from the list
+  if (remoteId != mainStreamId) {
+    // if not the main vidiel then show the user icon
+    toggleVisibility('#' + remoteId + '_no-video', true);
+  }
+});
+
+client.on("unmute-video", function (evt) {
+  toggleVisibility('#' + evt.uid + '_no-video', false);
+});
+
+// join a channel
+function joinChannel(channelName, uid, token) {
+  client.join(token, channelName, uid, function(uid) {
+      console.log("User " + uid + " join channel successfully");
+      createCameraStream(uid);
+      localStreams.camera.id = uid; // keep track of the stream uid 
+  }, function(err) {
+      console.log("[ERROR] : join channel failed", err);
+  });
+}
+
+// video streams for channel
+function createCameraStream(uid) {
+  var localStream = AgoraRTC.createStream({
+    streamID: uid,
+    audio: true,
+    video: true,
+    screen: false
+  });
+  localStream.setVideoProfile(cameraVideoProfile);
+  localStream.init(function() {
+    console.log("getUserMedia successfully");
+
+    localStream.play('local-video'); // play the given stream within the local-video div
+
+    // publish local stream
+    client.publish(localStream, function (err) {
+      console.log("[ERROR] : publish local stream error: " + err);
+    });
+  
+    enableUiControls(localStream); // move after testing
+    localStreams.camera.stream = localStream; // keep track of the camera stream for later
+  }, function (err) {
+    console.log("[ERROR] : getUserMedia failed", err);
+  });
+}
+
+// REMOTE STREAMS UI
+function addRemoteStreamMiniView(remoteStream){
+  var streamId = remoteStream.getId();
+  // append the remote stream template to #remote-streams
+  $('#remote-streams').append(
+    $('<div/>', {'id': streamId + '_container',  'class': 'remote-stream-container col'}).append(
+      $('<div/>', {'id': streamId + '_mute', 'class': 'mute-overlay'}).append(
+          $('<i/>', {'class': 'fas fa-microphone-slash'})
+      ),
+      $('<div/>', {'id': streamId + '_no-video', 'class': 'no-video-overlay text-center'}).append(
+        $('<i/>', {'class': 'fas fa-user'})
+      ),
+      $('<div/>', {'id': 'agora_remote_' + streamId, 'class': 'remote-video'})
+    )
+  );
+  remoteStream.play('agora_remote_' + streamId); 
+
+  var containerId = '#' + streamId + '_container';
+  $(containerId).dblclick(function() {
+    // play selected container as full screen - swap out current full screen stream
+    remoteStreams[mainStreamId].stop(); // stop the main video stream playback
+    addRemoteStreamMiniView(remoteStreams[mainStreamId]); // send the main video stream to a container
+    $(containerId).empty().remove(); // remove the stream's miniView container
+    remoteStreams[streamId].stop() // stop the container's video stream playback
+    remoteStreams[streamId].play('full-screen-video'); // play the remote stream as the full screen video
+    mainStreamId = streamId; // set the container stream id as the new main stream id
+  });
+}
+
+function leaveChannel() {
+  
+  client.leave(function() {
+    console.log("client leaves channel");
+    localStreams.camera.stream.stop() // stop the camera stream playback
+    client.unpublish(localStreams.camera.stream); // unpublish the camera stream
+    localStreams.camera.stream.close(); // clean up and close the camera stream
+    $("#remote-streams").empty() // clean up the remote feeds
+    //disable the UI elements
+    $("#mic-btn").prop("disabled", true);
+    $("#video-btn").prop("disabled", true);
+    $("#screen-share-btn").prop("disabled", true);
+    $("#exit-btn").prop("disabled", true);
+    // hide the mute/no-video overlays
+    toggleVisibility("#mute-overlay", false); 
+    toggleVisibility("#no-local-video", false);
+    // show the modal overlay to join
+    $("#modalForm").modal("show"); 
+  }, function(err) {
+    console.log("client leave failed ", err); //error handling
+  });
+}
---
+++ ui.js
@@ -0,0 +1,75 @@
+// UI buttons
+function enableUiControls(localStream) {
+
+    $("#mic-btn").prop("disabled", false);
+    $("#video-btn").prop("disabled", false);
+    $("#screen-share-btn").prop("disabled", false);
+    $("#exit-btn").prop("disabled", false);
+    $("#gesture-btn").prop("disabled", false);
+  
+    $("#mic-btn").click(function(){
+      toggleMic(localStream);
+    });
+  
+    $("#video-btn").click(function(){
+      toggleVideo(localStream);
+    });
+
+    $("#gesture-btn").click(function(){
+      toggleGesture();
+    });
+  
+    $("#exit-btn").click(function(){
+      console.log("so sad to see you leave the channel");
+      leaveChannel(); 
+    });
+  }
+
+  
+  function toggleBtn(btn){
+    btn.toggleClass('btn-dark').toggleClass('btn-danger');
+  }
+  
+  function toggleVisibility(elementID, visible) {
+    if (visible) {
+      $(elementID).attr("style", "display:block");
+    } else {
+      $(elementID).attr("style", "display:none");
+    }
+  }
+  
+  function toggleMic(localStream) {
+    toggleBtn($("#mic-btn")); // toggle button colors
+    $("#mic-icon").toggleClass('fa-microphone').toggleClass('fa-microphone-slash'); // toggle the mic icon
+    if ($("#mic-icon").hasClass('fa-microphone')) {
+      localStream.unmuteAudio(); // enable the local mic
+      toggleVisibility("#mute-overlay", false); // hide the muted mic icon
+    } else {
+      localStream.muteAudio(); // mute the local mic
+      toggleVisibility("#mute-overlay", true); // show the muted mic icon
+    }
+  }
+
+  function toggleGesture() {
+    toggleBtn($("#gesture-btn")); // toggle button colors
+    $("#gesture-icon").toggleClass('fas fa-american-sign-language-interpreting').toggleClass('fad fa-american-sign-language-interpreting'); // toggle the mic icon
+    if ($("#gesture-icon").hasClass('fas fa-american-sign-language-interpreting')) {
+      init(); // fire up the pose.js script
+
+    } else {
+      pause(); //stop the posenet model
+
+    }
+  }
+  
+  function toggleVideo(localStream) {
+    toggleBtn($("#video-btn")); // toggle button colors
+    $("#video-icon").toggleClass('fa-video').toggleClass('fa-video-slash'); // toggle the video icon
+    if ($("#video-icon").hasClass('fa-video')) {
+      localStream.unmuteVideo(); // enable the local video
+      toggleVisibility("#no-local-video", false); // hide the user icon when video is enabled
+    } else {
+      localStream.muteVideo(); // disable the local video
+      toggleVisibility("#no-local-video", true); // show the user icon when video is disabled
+    }
+  }
